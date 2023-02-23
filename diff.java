42ccb39090b9e94d1c653fd3f6065d294d848984
Mohit Choure <mohitchoure1234@gmail.com>
2023-02-23 23:01:06 +0530

    modified diff.java to return absolute value

--- diff.java
+++ diff.java
@@ -2,6 +2,9 @@ Public class Diff{
 	public static void main(String args[]){
 		int a=6;
 		int b=5;
-		system.out.println("The Diff is: " + (a-b));
+		if(a>=b)
+			system.out.println("The Diff is: " + (a-b));
+		else
+			system.out.println("The Diff is: " + (b-a));
 	}
 }
